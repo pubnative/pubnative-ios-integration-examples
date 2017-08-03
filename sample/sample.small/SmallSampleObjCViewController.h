3188ee26d2000a96f65637842cd3e14a285d0a93
Can Soykarafakılı <can.soykarafakili@gmail.com>
2017-08-03 15:24:11 +0200

    Small Sample integration for both Swift & Obejctive - C

---
+++ sample/sample.small/SmallSampleObjCViewController.h
@@ -0,0 +1,13 @@
+//
+//  SmallSampleObjCViewController.h
+//  sample
+//
+//  Created by Can Soykarafakili on 03.08.17.
+//  Copyright © 2017 Can Soykarafakili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SmallSampleObjCViewController : UIViewController
+
+@end
